640fe5dc27d6289db6fd43ca15866b82e3f23f77
cnelson20 <cjnelson2004@gmail.com>
2021-11-02 17:28:31 +0100

    code

--- main.c
+++ main.c
@@ -5,43 +5,31 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
-int printdir(char *s, int tabs);
+int printdir(char *s);
 int main(); 
 
 int main() {
-	printdir("./",1);
+	printdir("./");
 	return 0;
 }
-int printdir(char *s, int tabs) {
+int printdir(char *s) {
 	DIR *dirp = opendir(s);
-	struct dirent *p = readdir(dirp);
 	struct stat *stp;
 	unsigned int total_size = 0;
+	
 	printf("\nFiles:\n\n");
+	
+	struct dirent *p = readdir(dirp);
 	while (p) {
 		if (p->d_type == 8) {
 			stat(p->d_name,stp);
-			char *t = ctime(&stp->st_mtime);
-			*(strchr(t,'\n')) = '\0';
-			printf("%ho %ld %s\t%s\n",stp->st_mode % 01000,stp->st_size,t,p->d_name);
-			total_size += stp->st_size;
+			printf("%ho %*u %s\n",0/*stp->st_mode % 01000*/,0/*stp->st_size*/,p->d_name);
+			//total_size += stp->st_size;
 		}
 		p = readdir(dirp);
 	}
 	closedir(dirp);
-	dirp = opendir(s);
-	p = readdir(dirp);
 	printf("\nDirectories:\n\n");
-	while (p) {
-		if (p->d_type == 4) {
-			stat(p->d_name,stp);
-			char *t = ctime(&stp->st_mtime);
-			printf("%hho %ld %s %s\n",stp->st_mode,stp->st_size,t,p->d_name);
-			total_size += stp->st_size;
-		}
-		p = readdir(dirp);
-	}
-	closedir(dirp);
 	printf("\nTotal Size: %u\n",total_size);
 	return 0;
 }
