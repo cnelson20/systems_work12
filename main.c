a0426a13ca454dd1fbe90c9ed899a93b235cdd88
cnelson20 <cjnelson2004@gmail.com>
2021-11-03 11:17:55 -0400

    done

--- main.c
+++ main.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <string.h>
+#include <errno.h>
 
 int printdir(char *s);
-int main(); 
+int main(int argc, char *argv[]); 
 
-int main() {
-	printdir("./");
-	return 0;
+int main(int argc, char *argv[]) {
+  char dir[256];
+
+  unsigned short i;
+  for (i = 0; i < 256; i++) {dir[i] = '\0';}
+  
+  if (argc > 1) {strncpy(dir,argv[1],256-1);}
+  i = 0;
+  while (printdir(dir) == -1) {
+    if (i) {
+      printf("%s: %s\n",dir,strerror(errno));
+    }
+    printf("Please enter a directory name:\n");
+    fgets(dir,256,stdin);
+    *(strrchr(dir,'\n')) = '\0';
+    i = 1;
+  }
+  return 0;
 }
 int printdir(char *s) {
-	DIR *dirp = opendir(s);
-	struct stat stp;
-	unsigned int total_size = 0;
+  DIR *dirp = opendir(s);
+  if (dirp == NULL) {return -1;}
+  struct stat stp;
+  long unsigned int total_size = 0;
 	
-	printf("Stats for directory %s\n",s);
-	printf("\nDirectories:\n\n");
-	struct dirent *p = readdir(dirp);
-	while (p) {
-		if (p->d_type == 4) {
-		stat(p->d_name,&stp);
-		printf("%ho %s\n",stp.st_mode % 01000,p->d_name);
-		total_size += stp.st_size;
-		}
-		p = readdir(dirp);
-	}
-	rewinddir(dirp);
-	p = readdir(dirp);
-	printf("\nFiles:\n\n");
-	while (p) {
-		if (p->d_type == 8) {
-		stat(p->d_name,&stp);
-		printf("%ho %d %s\n",stp.st_mode % 01000,stp.st_size,p->d_name);
-		total_size += stp.st_size;
-		}
-		p = readdir(dirp);
-	}
-	closedir(dirp);
-	printf("\nTotal Size: %u\n",total_size);
-	return total_size;
+  printf("Stats for directory %s\n",s);
+  printf("\nDirectories:\n\n");
+  struct dirent *p = readdir(dirp);
+  while (p) {
+    if (p->d_type == 4) {
+      stat(p->d_name,&stp);
+      printf("%ho %ld %s\n",stp.st_mode % 01000,stp.st_size,p->d_name);
+      total_size += stp.st_size;
+    }
+    p = readdir(dirp);
+  }
+  rewinddir(dirp);
+  p = readdir(dirp);
+  printf("\nFiles:\n\n");
+  while (p) {
+    if (p->d_type == 8) {
+      stat(p->d_name,&stp);
+      printf("%ho %ld %s\n",stp.st_mode % 01000,stp.st_size,p->d_name);
+      total_size += stp.st_size;
+    }
+    p = readdir(dirp);
+  }
+  closedir(dirp);
+  printf("\nTotal Size: %lu\n",total_size);
+  return total_size;
 }
